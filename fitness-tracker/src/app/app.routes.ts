c7dca93b3c55b51e44932feb1bba20c7252a8c5d
Loges <sivalog25@gmail.com>
2024-07-17 10:40:28 +0530

    login module

--- fitness-tracker/src/app/app.routes.ts
+++ fitness-tracker/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'fitness-tracking',loadChildren:()=> import('./fitness-tracking/fitness-tracking.module').then(m=>m.FitnessTrackingModule)}
+];
---
+++ fitness-tracker/src/app/fitness-tracking/fitness-tracking.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FitnessTrackingRoutingModule } from './fitness-tracking-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FitnessTrackingRoutingModule
+  ]
+})
+export class FitnessTrackingModule { }
